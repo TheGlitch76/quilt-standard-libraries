4b27a80e183f65eae0d9afc620e812c5a79bd35d
glitch <glitch.g3431@gmail.com>
2024-06-28 11:40:21 -0500

    delete qfapi experiment

--- qfapi/makePatches.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# uses pretty much the same strategy as https://github.com/MinecraftForge/gitpatcher
-rm -rf patches
-mkdir patches
-cd quilt
-git format-patch --no-stat --zero-commit --full-index --no-signature -N -o ../patches origin/upstream
-cd ..
-git add -A patches
